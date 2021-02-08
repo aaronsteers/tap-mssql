8dd2abed7b5a3ffe5322de7b86ce10dc64fdf6a2
John Timeus <john.timeus@slalom.com>
2021-02-08 14:53:38 -0800

    add code comments

--- bin/tap-mssql.bat
+++ bin/tap-mssql.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
+REM setlocal limits context changes to just within batch file
 setlocal
+REM change current directory to the location of batch file
 cd /d %~dp0
+REM lein must be run in context of src folder
 cd ..\src
 lein run -m tap-mssql.core %*
